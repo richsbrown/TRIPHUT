2f71caaca2904b9a0c3f13f7f1c348124c78f63d
Richard S. Brown <richsb007@gmail.com>
2021-11-24 22:40:00 -0500

    adding additional signup tests

--- client/src/components/Screens/signupForm.test.js
+++ client/src/components/Screens/signupForm.test.js
@@ -1,10 +1,11 @@
 /* eslint-disable jest/valid-expect */
-import { render } from "@testing-library/react";
+import { act, fireEvent, render } from "@testing-library/react";
 import { expect } from "chai";
 import { Provider } from "react-redux";
 import store from '../../Redux/store';
 import {BrowserRouter} from 'react-router-dom';
 import Signup from "./signup";
+import formData  from './signup'
 
 
 describe('Signup Component Test', () => {
@@ -44,4 +45,70 @@ describe('Signup Component Test', () => {
         // eslint-disable-next-line no-unused-expressions
         expect(loginButtonLink).to.exist;
     });
+
+
+    it('changes email to the users input', async () => {
+        await act( async () => {
+            const { getByTestId } = render(
+                <Provider store ={store}>
+                <BrowserRouter>
+                    <Signup formData={formData}/>
+                </BrowserRouter>
+                </Provider>
+            );
+            const email = getByTestId('signupEmail');
+            const testInputEmail = 'testEmail@gmail.com';
+            await fireEvent.change(email, {target: {value: testInputEmail}})
+            expect(email.value).to.be.equal(testInputEmail);
+        });
+    });
+    
+    it('changes password to the users input', async () => {
+        await act( async () => {
+            const { getByTestId } = render(
+                <Provider store ={store}>
+                <BrowserRouter>
+                    <Signup formData={formData}/>
+                </BrowserRouter>
+                </Provider>
+            );
+            const password = getByTestId('signupPassword');
+            const testInputPassword = '123456';
+            await fireEvent.change(password, {target: {value: testInputPassword}})
+            expect(password.value).to.be.equal(testInputPassword);
+        });
+    });
+
+    it('changes fullname to the users input', async () => {
+        await act( async () => {
+            const { getByTestId } = render(
+                <Provider store ={store}>
+                <BrowserRouter>
+                    <Signup formData={formData}/>
+                </BrowserRouter>
+                </Provider>
+            );
+            const fullname = getByTestId('signupFullname');
+            const testInputFullname = 'Jon Doe';
+            await fireEvent.change(fullname, {target: {value: testInputFullname}})
+            expect(fullname.value).to.be.equal(testInputFullname);
+        });
+    });
+
+    it('changes username to the users input', async () => {
+        await act( async () => {
+            const { getByTestId } = render(
+                <Provider store ={store}>
+                <BrowserRouter>
+                    <Signup formData={formData}/>
+                </BrowserRouter>
+                </Provider>
+            );
+            const username = getByTestId('signupUsername');
+            const testInputUsername = 'JDoe';
+            await fireEvent.change(username, {target: {value: testInputUsername}})
+            expect(username.value).to.be.equal(testInputUsername);
+        });
+    });
+
 });
